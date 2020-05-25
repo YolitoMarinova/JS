937a284505d161a3f73a873c8ac22f07c1708db5
Yoana <marinova.m.y@gmail.com>
2020-05-25 22:49:55 +0300

    First 3 labs

---
+++ Labs/Syntax, Functions and Statements/03.SumOfNumbers.js
@@ -0,0 +1,14 @@
+function solve(n, m) {
+    let firstNumber = Number(n);
+    let secondNumber = Number(m);
+    let result;
+
+
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        result =result + index;
+    }
+
+    console.log(result);
+}
+
+solve('1','3');
